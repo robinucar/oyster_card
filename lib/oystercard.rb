ad540d4d5fb111be831b1dac43f41680cfdff212
robinucar <mehmet.ucar@imediapro.co.uk>
2021-12-16 16:35:23 +0000

    stored journey, stored journey , clean journey, removed entry_station instance variable

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,10 +2,11 @@ class Oystercard
   BALANCE_LIMIT = 90
   MINIMUM_LIMIT = 1
   MINIMUM_FARE = 2
-  attr_reader :balance, :entry_station
+  attr_accessor :balance, :journeys, :journey
   def initialize(balance = 5)
     @balance = balance
-    @entry_station
+    @journeys = []
+    @journey = {}
   end
   def top_up(amount)
     raise "Error: £#{BALANCE_LIMIT} limit reached" if balance + amount > BALANCE_LIMIT
@@ -14,16 +15,25 @@ class Oystercard
   def deduct(amount)
     @balance -= amount
   end
-  def touch_in(station)
+  def touch_in(entry_station)
     raise 'You haven\'t got enough money' if balance < MINIMUM_LIMIT
-    @entry_station = station
+    @journey = {:entry_station => entry_station}
   end
-  def touch_out
+  def touch_out(exit_station)
     deduct(MINIMUM_FARE)
-    @entry_station = false
+    @journey[:exit_station] = exit_station
+    store_journey
+    clear_journey
+  end
+  def store_journey
+    @journeys << @journey.dup  # store copy of journey to the journeys array
+  end
+  def clear_journey
+    @journey[:entry_station] = nil
+    @journey[:exit_station] = nil
   end
   def in_journey?
-    !!@entry_station
+    !!@journey[:entry_station]
   end
-  
+  private :deduct, :store_journey, :clear_journey
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,7 +1,9 @@
 require 'oystercard'
 
 context Oystercard do
-  let(:station){double(:station)}
+  let(:start_station){double :start_station}
+  let(:end_station){double :end_station }
+  let(:journey){ { :entry_station => :start_station, :exit_station => :end_station } }
   it 'should respond to #initialize' do
     oystercard = Oystercard.new
     expect(oystercard).to respond_to {'initialize'}
@@ -20,36 +22,50 @@ context Oystercard do
     end
   end
 
-  describe '#deduct' do
-    it { is_expected.to respond_to(:deduct).with(1).argument }
+=begin describe '#deduct' do
+ it { is_expected.to respond_to(:deduct).with(1).argument }
     it 'deducts amount from balance' do
       expect { subject.deduct 1 }.to change {subject.balance}.by -1
     end
   end
+=end
   describe '#touch_in' do
    it 'should change the status of in_journey to true' do
-    subject.touch_in(station)
+    subject.touch_in(:start_station)
     expect(subject.in_journey?).to eq true
    end
     it 'should raise error if below minimum limit' do
-      balance = subject.balance
-      subject.deduct(balance)
+      subject.balance = Oystercard::MINIMUM_LIMIT - 1
       message = 'You haven\'t got enough money'
-      expect {subject.touch_in(station)}.to raise_error message
+      expect {subject.touch_in(:start_station)}.to raise_error message
     end
     it 'should store the entry station' do
-      expect(subject.touch_in(station)).to eq station
+      subject.touch_in(:start_station)
+      expect(subject.journey[:entry_station]).to eq :start_station
     end
   end
   describe '#touch_out' do
     it 'should change the status of in_journey to false' do
-      subject.touch_in(station)
-      subject.touch_out
+      subject.touch_in(:start_station)
+      subject.touch_out(:end_station)
       expect(subject.in_journey?).to eq false
     end
     it 'should deduct fare from balance' do
       fare = Oystercard::MINIMUM_FARE
-      expect {subject.touch_out}.to change{subject.balance}.by(-fare)
+      expect {subject.touch_out(:end_station)}.to change{subject.balance}.by(-fare)
     end
+    it 'should store the exit station' do
+      subject.touch_in(:start_station)
+      subject.touch_out(:end_station)
+      expect(subject.journeys[0][:exit_station]).to eq :end_station
+    end
+  end
+  it 'has empty journey history as default' do
+    expect(subject.journeys).to be_empty
+  end
+  it 'stores all journeys' do
+    subject.touch_in(:start_station)
+    subject.touch_out(:end_station)
+    expect(subject.journeys).to include journey
   end
 end
