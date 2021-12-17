d650cb7f834a818f47ecb625431c6ed050bd5c8d
robinucar <mehmet.ucar@imediapro.co.uk>
2021-12-17 21:29:53 +0000

    store end journey, store all journeys, complete method

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,11 +1,22 @@
 class Journey
-  attr_reader :complete, :stations
+  attr_reader :complete, :stations, :journeys_list
   def initialize
     @complete = false #it is false by default
     @stations = {enter: nil, exit: nil}
+    @journeys_list = []
   end
   def start(station)
     @stations[:enter] = station
     @complete = false
+  end
+  def end(station)
+    @stations[:exit] = station
+    @complete = true
+  end
+  def complete?
+    @complete
+  end
+  def store_journey
+    @journeys_list << @stations
   end
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -3,13 +3,13 @@ require 'journey'
 describe Journey do
   subject(:journey) { described_class.new }
   let(:station) {double :station}
-  describe 'complete?' do
+  describe '#complete?' do
     it 'returns false when journey has not completed yet' do
       journey.start(station)
       expect(journey.complete).to eq false
     end
   end
-  describe 'start'do
+  describe '#start' do
     before(:each) do
       journey.start('Bank')
     end
@@ -17,4 +17,17 @@ describe Journey do
       expect(journey.stations).to eq({enter: 'Bank', exit: nil})
     end
   end
+
+  describe '#end' do
+    before(:each) do
+      journey.start('Bank')
+      journey.end('Finsburry Park')
+    end
+    it 'store the exit station' do
+      expect(journey.stations).to eq({enter: 'Bank', exit: 'Finsburry Park'})
+    end
+    it 'store completed journey' do
+      expect(journey.store_journey).to eq([{enter: 'Bank', exit: 'Finsburry Park'}])
+    end
+  end 
 end
