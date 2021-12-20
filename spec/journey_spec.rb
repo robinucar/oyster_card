f2adb52c9fc5297193e0a8c7ed352883388c141e
robinucar <mehmet.ucar@imediapro.co.uk>
2021-12-20 19:50:04 +0000

    removed comments

--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -12,24 +12,6 @@ describe Journey do
       expect(journey.complete).to eq false
     end
   end
-=begin
-describe '#end' do
-    before(:each) do
-      journey.start('Bank')
-      journey.end('Finsburry Park')
-    end
-    it 'store completed journey' do
-      expect(journey.journeys_list).to eq([{enter: 'Bank', exit: 'Finsburry Park'}])
-    end
-  end
-=end
-=begin
-describe '#clear_journey' do
-    it 'should clear enter and exit stations' do
-      expect(journey.stations).to eq({enter: nil, exit: nil})
-    end
-  end
-=end
   describe '#fare' do
    it 'should return the fare after touch in and touch out' do
     journey.entry_station('Bank')
